b712afa0f8dfa2b2b24ddef09970f5769d199b70
Damir Arbula <damir.arbula@gmail.com>
2016-10-11 12:06:03 +0200

    size

--- vendor/reveal-initialize.js
+++ vendor/reveal-initialize.js
@@ -10,8 +10,8 @@ Reveal.initialize({
     // The "normal" size of the presentation, aspect ratio will be preserved
     // when the presentation is scaled to fit different resolutions. Can be
     // specified using percentage units.
-    width: 1024,
-    height: 768,
+    width: 1152,
+    height: 864,
 
     // Factor of the display size that should remain empty around the content
     margin: 0.07,
